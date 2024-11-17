fbb40687a2b458d7641fb0321e393d92bd35c671
Aniket Malpure <aniketmalpure@ufl.edu>
2024-11-17 00:55:16 -0500

    flask code and pipeline added

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 # This function saves the object on the disk
 def save_object(file_path, obj):
@@ -16,5 +18,36 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+# To train the model on the provided algorithms
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            # Getting a model individually
+            model = list(models.values())[i]
+            param=params[list(models.keys())[i]]
+            # Applying grid search cv to get best params for each algorithm
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+            # Training with that model
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+            #y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            #train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+#
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
